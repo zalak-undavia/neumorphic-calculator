1fb69a9446ee085a202fe4aaa12a7bc1bbf8db68
Zalak Undavia <zundavia@gmail.com>
2024-04-06 18:04:56 +0530

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCK8ZosXKZI1AYihbJUr7fyQ0CqQVyqGio",
+    authDomain: "calculator-2c6cc.firebaseapp.com",
+    projectId: "calculator-2c6cc",
+    storageBucket: "calculator-2c6cc.appspot.com",
+    messagingSenderId: "160094460937",
+    appId: "1:160094460937:web:cd809a70eb27f183beacd8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
